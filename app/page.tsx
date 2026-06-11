05755e3007773da25b5f2d14a29936672cd73d1e
Jing Duan <hanbing6228@gmail.com>
2026-06-11 16:28:07 -0400

    Update page.tsx to ensure proper hydration

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,16 @@
+'use client';
+
+import { useEffect, useState } from 'react';
 import Portal from '@/components/portal/Portal';
 
 export default function Home() {
+  const [mounted, setMounted] = useState(false);
+
+  useEffect(() => {
+    setMounted(true);
+  }, []);
+
+  if (!mounted) return null;
+
   return <Portal />;
 }
